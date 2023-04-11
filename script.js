b07e8ed0763cffc71f90a770e234e19f66252f7a
Samuel Duncan <samuel.ira.duncan@gmail.com>
2023-04-10 20:14:49 -0400

    Update game function to play a 5 round game
    playGame function updates score for player and computer.
    getWinner declares winner of 5 rounds.

--- script.js
+++ script.js
@@ -15,32 +15,57 @@ function getPlayerChoice() {
   const formattedChoice = playerChoice.toLowerCase();
   return formattedChoice;
 }
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
+
+// Keep score
+let playerScore = 0;
+let computerScore = 0;
 
 // Play a single round
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === 'rock' && computerSelection === 'rock') {
-    return 'It\'s a tie! Play again.';
-  } if (playerSelection === 'rock' && computerSelection === 'paper') {
+function playRound(playerSelection = getPlayerChoice(), computerSelection = getComputerChoice()) {
+  console.log(`Player choice: ${playerSelection}`);
+  console.log(`Computer choice: ${computerSelection}`);
+
+  if (playerSelection === 'rock' && computerSelection === 'paper') {
+    computerScore++;
     return 'Computer wins! Paper beats rock.';
   } if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    playerScore++;
     return 'Player wins! Rock beats scissors.';
   } if (playerSelection === 'paper' && computerSelection === 'rock') {
+    playerScore++;
     return 'Player wins! Paper beats rock.';
-  } if (playerSelection === 'paper' && computerSelection === 'paper') {
-    return 'It\'s a tie! Play again.';
   } if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    computerScore++;
     return 'Computer wins! Scissors beats paper.';
   } if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    computerScore++;
     return 'Computer wins! Rock beats scissors.';
   } if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    playerScore++;
     return 'Player wins! Scissors beats paper.';
-  } if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+  } if (playerSelection === computerSelection) {
     return 'It\'s a tie! Play again.';
   }
 }
 
 // Play a 5 round game
+function game() {
+  for (let i = 0; i < 5; i++) {
+    console.log(playRound());
+  }
+}
+
+game();
 
 // Declare winner
+function getWinner() {
+  if (playerScore > computerScore) {
+    console.log(`Player has won the game ${playerScore} to ${computerScore}`);
+  } else if (playerScore < computerScore) {
+    console.log(`Computer has won the game ${computerScore} to ${playerScore}`);
+  } else {
+    console.log('The game has ended in a tie. Please play again.');
+  }
+}
+
+getWinner();
